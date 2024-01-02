1afcd19d528eac963f24b921de095405af4d3e12
Anjali Gupta <anjaligupta0621@gmail.com>
2024-01-02 17:45:07 -0500

    modified homescreen - avatar and search

--- CoffeeBuddy/src/components/HeaderBar.tsx
+++ CoffeeBuddy/src/components/HeaderBar.tsx
@@ -1,13 +1,42 @@
 /* eslint-disable prettier/prettier */
 
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { COLORS, FONTFAMILY, FONTSIZE, SPACING } from '../theme/theme';
+import GradientBGIcon from './GradientBGIcon';
+import ProfilePic from './ProfilePic';
 
-export default function HeaderBar() {
+interface HeaderBarProps {
+    title?: string;
+}
 
+const HeaderBar: React.FC<HeaderBarProps> = ({ title }) => {
     return (
-        <View>
-            <Text> HeaderBar </Text>
+        <View style={styles.HeaderContainer}>
+            <GradientBGIcon
+                name='menu'
+                color={COLORS.primaryLightGreyHex}
+                size={FONTSIZE.size_16}
+            />
+            <Text style={styles.HeaderText}> {title} </Text>
+            <ProfilePic />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    HeaderContainer: {
+        padding: SPACING.space_30,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    HeaderText: {
+        fontFamily: FONTFAMILY.poppins_semibold,
+        fontSize: FONTSIZE.size_20,
+        color: COLORS.primaryWhiteHex,
+    },
+});
+
+export default HeaderBar;
+
